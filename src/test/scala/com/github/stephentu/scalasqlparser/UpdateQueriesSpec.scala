26606af1df684ee0445eea5c9e9594d331c2ec54
KUOKA Yusuke <ykuoka@gmail.com>
2012-12-17 17:42:43 +0900

    Added specs for UPDATE statements which uses a lot of parenthesises in WHERE clause

--- src/test/scala/com/github/stephentu/scalasqlparser/UpdateQueriesSpec.scala
+++ src/test/scala/com/github/stephentu/scalasqlparser/UpdateQueriesSpec.scala
@@ -19,6 +19,19 @@ ON
 WHERE
   u.`user_id` = 1
       """.stripMargin
+    // Oracle specifics function `to_timestamp` is used
+    val q3 =
+      """
+UPDATE
+  `users`
+SET
+  `id1`=1000,
+  `id2`='2000',
+  `nickname`='mumoshu',
+  `updated_time`=to_timestamp('2012-12-14 12:45:39.000','yyyy-MM-dd hh24:mi:ss.ff3')
+WHERE
+  ((`users`.`id1`=1000) and (`users`.`id2`=2000))
+      """.stripMargin
   }
 
   "SQLParser" should {
@@ -30,6 +43,11 @@ WHERE
     "parse query2" in {
       val parser = new SQLParser
       val r = parser.parse(Queries.q2)
+      r should beSome
+    }
+    "parse query3" in {
+      val parser = new SQLParser
+      val r = parser.parse(Queries.q3)
       r should beSome
     }
   }
